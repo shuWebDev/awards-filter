104294b03d55386bc131395b8f662ebdce37226f
Erik K Fecher <fecherea@shu.edu>
2019-12-09 09:56:08 -0500

    update to package dependency and fix for filtering to prevent bothering to filter on an already zero set

--- src/util/searchfunctions.ts
+++ src/util/searchfunctions.ts
@@ -33,27 +33,30 @@ function generateResultListing (awardData:Services.AwardData[], filterBoxText:st
 function searchByProgram(awardData:Services.AwardData[], selectedPrograms:string[]) {
   let returnableResultSet:Services.AwardData[] = [];
 
-  
-  // NOTE: cycle through the records
-  //console.log(selectedPrograms.length);
-  if(selectedPrograms.length) {
-    for(let i=0; i<awardData.length; i++) {
-      // NOTE: see if any of our selected programs match what the eligibility program is for this record
-      for(let program in selectedPrograms) {
-        // NOTE: if we find a match
-        if(awardData[i].eligibilityProgram === selectedPrograms[program]) {
-          // NOTE: push the record to the result set, stop checking if other programs match, and break out to check the next record 
-          returnableResultSet.push(awardData[i]);
-          break;
+  if(awardData.length) {
+    // NOTE: cycle through the records
+    //console.log(selectedPrograms.length);
+    if(selectedPrograms.length) {
+      for(let i=0; i<awardData.length; i++) {
+        // NOTE: see if any of our selected programs match what the eligibility program is for this record
+        for(let program in selectedPrograms) {
+          // NOTE: if we find a match
+          if(awardData[i].eligibilityProgram === selectedPrograms[program]) {
+            // NOTE: push the record to the result set, stop checking if other programs match, and break out to check the next record 
+            returnableResultSet.push(awardData[i]);
+            break;
+          }
         }
       }
+    } else {
+      // NOTE: in this case user unchecks all program category boxes, set results back to all records
+      returnableResultSet = awardData;
     }
-  } else {
-    // NOTE: in this case user unchecks all program category boxes, set results back to all records
-    returnableResultSet = awardData;
-  }
 
-  return returnableResultSet;
+    return returnableResultSet;
+  } else {
+    return awardData;
+  }
 }
 
 
@@ -64,44 +67,53 @@ function searchByText(awardData: Services.AwardData[], filterBoxText: string): S
   let t0 = performance.now();
   let resultSet: Services.AwardData[] = [];
 
-  for(let item in awardData) {
-    const keys: string[] = Object.keys(awardData[item]);
-    for(let i=0; i<keys.length; i++) {
-      if(typeof keys[i] === "string") {
-        let prop:string = UtilServices.prop(awardData[item],keys[i]);
-        if(prop.toString().toUpperCase().includes(filterBoxText.toUpperCase())) {
-          if(!resultSet.includes(awardData[item])) {
-            resultSet.push(awardData[item]);
+  if(awardData.length) {
+    for(let item in awardData) {
+      const keys: string[] = Object.keys(awardData[item]);
+      for(let i=0; i<keys.length; i++) {
+        if(typeof keys[i] === "string") {
+          let prop:string = UtilServices.prop(awardData[item],keys[i]);
+          if(prop.toString().toUpperCase().includes(filterBoxText.toUpperCase())) {
+            if(!resultSet.includes(awardData[item])) {
+              resultSet.push(awardData[item]);
+            }
           }
         }
       }
     }
+    let t1 = performance.now();
+    console.log(`searchByText took ${t1-t0} milliseconds to complete.`);
+    return resultSet;
+  } else {
+    // NOTE: no action, the result set is already zero length
+    return awardData;
   }
-  let t1 = performance.now();
-  console.log(`searchByText took ${t1-t0} milliseconds to complete.`);
-  return resultSet;
 }
 
 function searchByAmount(awardData:Services.AwardData[], minAwardAmount:number) {
   let returnableResultSet:Services.AwardData[] = []; 
 
-  for(let i=0; i<awardData.length; i++) {
-    // NOTE: value is a string that can either be numeric or "variable"
-    
-    // NOTE: if the value is a number, test if it fits what we need
-    if(typeof awardData[i].amount === "number") {
-      if(parseInt(awardData[i].amount) >= minAwardAmount) {
-        returnableResultSet.push(awardData[i]);
-      }
-    } else {
-      // NOTE: add the variable ones in too if they are the same category, they still may be relevant
-      if(typeof awardData[i].amount === "string") {
-        returnableResultSet.push(awardData[i]);
-      }
-    } 
-  }
+  if(awardData.length) {
+    for(let i=0; i<awardData.length; i++) {
+      // NOTE: value is a string that can either be numeric or "variable"
+      
+      // NOTE: if the value is a number, test if it fits what we need
+      if(typeof awardData[i].amount === "number") {
+        if(parseInt(awardData[i].amount) >= minAwardAmount) {
+          returnableResultSet.push(awardData[i]);
+        }
+      } else {
+        // NOTE: add the variable ones in too if they are the same category, they still may be relevant
+        if(typeof awardData[i].amount === "string") {
+          returnableResultSet.push(awardData[i]);
+        }
+      } 
+    }
 
-  return returnableResultSet;
+    return returnableResultSet;
+  } else {
+    return awardData;
+  }
 }
 
 export { generateResultListing }
